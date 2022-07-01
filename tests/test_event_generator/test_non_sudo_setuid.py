2584e254bc4a564af62324c71ddb7aa7c6a125cb
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-07-01 18:43:30 +0200

    Add a few process related tests

---
+++ tests/test_event_generator/test_non_sudo_setuid.py
@@ -0,0 +1,51 @@
+import pytest
+from sinspqa import sinsp
+from sinspqa.sinsp import assert_events
+
+sinsp_filters = ["-f", "evt.type=setuid"]
+
+containers = [{
+    'sinsp': sinsp.container_spec(args=sinsp_filters),
+    'generator': {
+        'image': 'falcosecurity/event-generator',
+        'args': ['run', 'syscall.NonSudoSetuid'],
+        'privileged': True,
+    },
+}]
+
+
+@pytest.mark.parametrize("run_containers", containers, indirect=True)
+def test_non_sudo_setuid(run_containers):
+    sinsp_container = run_containers['sinsp']
+
+    generator_container = run_containers['generator']
+    generator_container.wait()
+
+    expected_events = [
+        {
+            "evt.args": "uid=2(<NA>) ",
+            "evt.dir": ">",
+            "evt.type": "setuid",
+            "proc.name": "child",
+        },
+        {
+            "evt.args": "res=0 ",
+            "evt.dir": "<",
+            "evt.type": "setuid",
+            "proc.name": "child",
+        },
+        {
+            "evt.args": "uid=0(<NA>) ",
+            "evt.dir": ">",
+            "evt.type": "setuid",
+            "proc.name": "child",
+        },
+        {
+            "evt.args": "res=-1(EPERM) ",
+            "evt.dir": "<",
+            "evt.type": "setuid",
+            "proc.name": "child",
+        },
+    ]
+
+    assert_events(expected_events, sinsp_container)
